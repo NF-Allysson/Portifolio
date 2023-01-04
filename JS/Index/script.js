7b85cdc8e2a04b9ac99aabc29c74713a3f62098c
Allysson Ferreira <94935613+NF-Allysson@users.noreply.github.com>
2023-01-03 21:32:30 -0300

    ..

--- JS/Index/script.js
+++ JS/Index/script.js
@@ -1,25 +1,32 @@
-$(document).ready(function(){
-    $('#menu-icon').click(function(){
-        $(this).toggleClass('fa-times')
-        $('.nav-barra').toggleClass('nav-toggle')
-    })
-})
+// $(document).ready(function(){
+//     $('#menu-icon').click(function(){
+//         $(this).toggleClass('fa-times')
+//         $('.nav-barra').toggleClass('nav-toggle')
+//     })
+// })
 
 
-let men2 = document.getElementById("part2")
-let men = document.getElementById("part1") 
-let bar = document.getElementById("header")
+// let men2 = document.getElementById("part2")
+// let men = document.getElementById("part1") 
+// let bar = document.getElementById("header")
 
-men2.addEventListener('wheel', () => {
-    bar.style.display = "flex"
-    bar.style.position = "fixed"
-})
-men.addEventListener('wheel', () => {
-    bar.style.display = "flex"
-    bar.style.position = "relative"
-})
+// men2.addEventListener('wheel', () => {
+//     bar.style.display = "flex"
+//     bar.style.position = "fixed"
+// })
+// men.addEventListener('wheel', () => {
+//     bar.style.display = "flex"
+//     bar.style.position = "relative"
+// })
 
+// var campoteste = document.getElementById("togllebut")
+// var tema = document.getElementById("turma")
+// tema.addEventListener('click', Tematela)
 
-
-
+// function Tematela(){
+//     campoteste.style.background = "black"
+//     var claro = document.getElementById("turn")
+//     claro.classList.toggle("fa-moon")
+//     claro.classList.add("fa-sun")
+// }
 
